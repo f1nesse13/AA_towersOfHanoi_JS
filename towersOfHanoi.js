b95c40fff6b0551f0943d51a7f75dff0b2c42e42
Joe Pell <jpell3130@gmail.com>
2019-06-10 00:38:06 -0400

    formatting

--- towersOfHanoi.js
+++ towersOfHanoi.js
@@ -1,66 +1,62 @@
 class Game {
-  constructor () {
-    this.towers = [[3,2,1], [], []]
+  constructor() {
+    this.towers = [[3, 2, 1], [], []];
   }
 
-  printBoard () {
+  printBoard() {
     console.log(JSON.stringify(this.towers));
   }
 
-  promptMove (reader, callback) {
+  promptMove(reader, callback) {
     this.printBoard();
 
-    reader.question("Choose a starting tower: ", start => {
-      let startTower = parseInt(start);
-      reader.question("Choose an ending tower: ", end => {
-        let endTower = parseInt(end);
+    reader.question('Choose a starting tower: ', start => {
+      const startTower = parseInt(start);
+      reader.question('Choose an ending tower: ', end => {
+        const endTower = parseInt(end);
         callback(startTower, endTower);
       });
     });
-    
   }
 
-  validMove (startTower, endTower) {
-    let startDisc = this.towers[startTower]
-    let endDisc = this.towers[endTower]
-    if ((startDisc[startDisc.length - 1] > endDisc[endDisc.length - 1]) || startDisc.length === 0) {
+  validMove(startTower, endTower) {
+    const startDisc = this.towers[startTower];
+    const endDisc = this.towers[endTower];
+    if (startDisc[startDisc.length - 1] > endDisc[endDisc.length - 1] || startDisc.length === 0) {
       return false;
-    } else {
-      return true;
     }
+    return true;
   }
 
-  move (startTower, endTower) {
-    if(this.validMove(startTower, endTower)) {
+  move(startTower, endTower) {
+    if (this.validMove(startTower, endTower)) {
       this.towers[endTower].push(this.towers[startTower].pop());
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
 
-  isWon () {
+  isWon() {
     if (this.towers[1].length === 3 || this.towers[2].length === 3) {
-      console.log("You win!")
+      console.log('You win!');
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
 
-  run (reader, completionCallback) {
+  run(reader, completionCallback) {
     this.promptMove(reader, (startTower, endTower) => {
       if (!this.move(startTower, endTower)) {
-        console.log("Invalid move!")
+        console.log('Invalid move!');
       }
-    if (!this.isWon()) {
-      this.run(reader, completionCallback);
-    } else {
-      this.printBoard();
-      console.log("You win!")
-      completionCallback();
-    }
-  });
-}
+      if (!this.isWon()) {
+        this.run(reader, completionCallback);
+      } else {
+        this.printBoard();
+        console.log('You win!');
+        completionCallback();
+      }
+    });
+  }
 }
 module.exports = Game;
